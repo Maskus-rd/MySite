ac14a30787236c1b261ac3c37ac284c321a153d3
Ryan <80864263+Maskus-rd@users.noreply.github.com>
2023-11-29 18:08:04 -0800

    Update and rename index.jsx to index.js
    changing the extension back to .js after learning the the difference is that mostly of preference.

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MyApp = () => (
+  <div>
+    <h1>My Website</h1>
+  </div>
+);
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
