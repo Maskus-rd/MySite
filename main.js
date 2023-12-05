7081385cb53ec3b01ce253bed365e1d3269900c5
Ryan <80864263+Maskus-rd@users.noreply.github.com>
2023-12-04 19:25:31 -0800

    Update main.js
    adding my first constants

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // This is my websites .js file
+
+
+const numButtons = document.querrySelectorAll('[data-number]')
+const opperationButtons = document.querrySelectorAll('[data-opperation]')
+const equalsButtons = document.querrySelector('[data-equals]')
+const deleteButtons = document.querrySelector('[data-delete]')
+const clearButtons = document.querrySelectorAll('[data-clear]')
+const previousOperandTextElement = document.querrySelector('[data-Previous-operand]')
+const currentOperandTextElement = document.querrySelector('[data-current-operand]')
